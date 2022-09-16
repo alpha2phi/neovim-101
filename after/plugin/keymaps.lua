0ae5be705d5243144eb06e8ab813c317fe70dea4
Meng Wang <mengwangk@gmail.com>
2022-09-16 18:57:04 +0800

    feat: terminal and shell (#10)
    Co-authored-by: alpha2phi <alpha2phi@gmail.com>

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -19,3 +19,7 @@ keymap("n", "<Leader>t", '<CMD>lua require("FTerm").toggle()<CR>')
 keymap("t", "<Leader>t", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
 
 -- CSI u mappings test
+keymap("n", "<Tab>", '<CMD>lua print([[ Tab is pressed ]])<CR>')
+keymap("n", "<C-i>", '<CMD>lua print([[ Ctr-i is pressed ]])<CR>')
+keymap("n", "<C-Enter>", '<CMD>lua print([[ Ctr-Enter is pressed ]])<CR>')
+keymap("n", "<C-S-p>", '<CMD>lua print([[ Ctr-Shift-P is pressed ]])<CR>')
