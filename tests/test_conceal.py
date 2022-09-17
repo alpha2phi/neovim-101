22867f57c644ed3cafd83cc7d1571a2fb740d76e
alpha2phi <alpha2phi@gmail.com>
2022-09-17 22:59:58 +0800

    feat: treesitter

---
+++ tests/test_conceal.py
@@ -0,0 +1,16 @@
+"""Test conceal."""
+
+x = 1
+y = 2
+
+if x >= y:
+    print("x >= y")
+
+if not True:
+    print("not true")
+
+if 1 in [1,2,3]:
+    print("exists")
+
+def sum(x: int, y: float) -> float:
+    return x + y
