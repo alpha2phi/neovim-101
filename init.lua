7712ecee987298877e188d20ebad5d4a9f47e59e
Meng Wang <mengwangk@gmail.com>
2022-10-21 11:57:26 +0800

    11 lsp (#17)
    * 10 osc52 (#15) (#16)
    
    * feat: osc52
    
    * feat: osc52 wip
    
    * feat: osc52
    
    * feat: nvim-osc52
    
    * feat: smart yank
    
    Co-authored-by: alpha2phi <alpha2phi@gmail.com>
    
    Co-authored-by: alpha2phi <alpha2phi@gmail.com>
    
    * feat: Lua LSP
    
    * feat: LSP config
    
    * feat: lsp
    
    Co-authored-by: alpha2phi <alpha2phi@gmail.com>

--- init.lua
+++ init.lua
@@ -124,7 +124,7 @@ local function plugins(use)
 	use({
 		"adelarsq/image_preview.nvim",
 		config = function()
-			require("image_preview").setup({})
+			require("image_preview").setup()
 		end,
 	})
 
@@ -380,6 +380,47 @@ local function plugins(use)
 		end,
 	})
 
+	-- LSP
+	--use({
+	--	"ray-x/navigator.lua",
+	--	requires = {
+	--		{ "ray-x/guihua.lua", run = "cd lua/fzy && make" },
+	--		{ "neovim/nvim-lspconfig" },
+	--	},
+	--	config = function()
+	--		require("navigator").setup()
+	--	end,
+	--	disable = true,
+	--})
+
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+		config = function()
+			local lsp = require("lsp-zero")
+			lsp.preset("recommended")
+			lsp.setup()
+			lsp.nvim_workspace()
+		end,
+	})
+
 	-- Bootstrap Neovim
 	if packer_bootstrap then
 		print("Neovim restart is required after installation!")
@@ -392,3 +433,6 @@ packer_init()
 local packer = require("packer")
 packer.init(conf)
 packer.startup(plugins)
+
+-- LSP configuration
+--require("config.lsp")
---
+++ lua/config/lsp/lua.lua
@@ -0,0 +1,42 @@
+local root_files = {
+  ".luarc.json",
+  ".luacheckrc",
+  ".stylua.toml",
+  "stylua.toml",
+  "selene.toml",
+  ".git",
+}
+
+local bin_name = "lua-language-server"
+local cmd = { bin_name }
+
+if vim.fn.has("win32") == 1 then
+  cmd = { "cmd.exe", "/C", bin_name }
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.lsp.start({
+      name = "sumneko",
+      cmd = cmd,
+      root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
+  end,
+})
