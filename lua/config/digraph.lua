16beac4672a799437c6d5f049e5f01e3a02f98e7
Meng Wang <mengwangk@gmail.com>
2022-12-16 18:35:12 +0800

    feat: symbols and icons (#25)
    Co-authored-by: alpha2phi <alpha2phi@gmail.com>

--- lua/config/digraph.lua
+++ lua/config/digraph.lua
@@ -2,5 +2,5 @@ local M = {}
 
 local keymap = vim.keymap.set
 
-keymap("i", "<C-k><C-k>", "<cmd>lua require'better-digraphs'.digraphs('i')<cr>", {})
-keymap("n", "r<C-k><C-k>", "<cmd>lua require'better-digraphs'.digraphs('r')<cr>", {})
+keymap("i", "<C-k><C-k>", "<cmd>lua require'better-digraphs'.digraphs('insert')<cr>", {})
+keymap("n", "r<C-k><C-k>", "<cmd>lua require'better-digraphs'.digraphs('normal')<cr>", {})
--- lua/utils/icons.lua
+++ lua/utils/icons.lua
@@ -1,6 +1,6 @@
 local icons = require("nvim-web-devicons").get_icons()
 
-vim.pretty_print(vim.tbl_count(icons))
+print("No of icons: ", vim.tbl_count(icons))
 for _, v in pairs(icons) do
-  print(v.name, v.icon)
+	print(v.name, v.icon)
 end
