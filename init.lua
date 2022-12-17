b1afa769aa8dfe9a964d837ba6e1b08fe737929d
alpha2phi <alpha2phi@gmail.com>
2022-12-17 21:37:20 +0800

    chore: testing new features

--- init.lua
+++ init.lua
@@ -625,6 +625,21 @@ local function plugins(use)
     },
   }
 
+  -- ChatGTP
+  use({
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup({
+        -- optional configuration
+      })
+    end,
+    requires = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  })
+
   -- Bootstrap Neovim
   if packer_bootstrap then
     print("Neovim restart is required after installation!")
