22ea4590da3a7aa8477ff4abe9c9783f9bddd02c
alpha2phi <alpha2phi@gmail.com>
2022-12-30 20:37:22 +0800

    feat: file management

--- init.lua
+++ init.lua
@@ -655,6 +655,15 @@ local function plugins(use)
 
   -- File explorer
   use { "elihunter173/dirbuf.nvim" }
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
   -- Bootstrap Neovim
   if is_bootstrap then
